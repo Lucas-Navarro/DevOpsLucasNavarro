f3a3a0ba858b7c130e710c4d781f8dde0673b62a
Lucas-Navarro <lucasr6out@gmail.com>
2025-03-18 10:57:40 -0300

    Atualizando e definindo os controladores e acesso ao banco

---
+++ DevOpsLucasNavarro/Data/DevOpsLucasNavarroContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DevOpsLucasNavarro.Models;
+
+namespace DevOpsLucasNavarro.Data
+{
+    public class DevOpsLucasNavarroContext : DbContext
+    {
+        public DevOpsLucasNavarroContext (DbContextOptions<DevOpsLucasNavarroContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DevOpsLucasNavarro.Models.Aluno> Aluno { get; set; } = default!;
+    }
+}
--- DevOpsLucasNavarro/Program.cs
+++ DevOpsLucasNavarro/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using DevOpsLucasNavarro.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DevOpsLucasNavarroContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevOpsLucasNavarroContext") ?? throw new InvalidOperationException("Connection string 'DevOpsLucasNavarroContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
